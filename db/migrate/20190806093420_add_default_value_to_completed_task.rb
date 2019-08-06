47f75d8137a29cd35687c28e31b848e87aaeb45b
Tarik Graoui <tarik.graoui@gmail.com>
2019-08-06 11:42:28 +0200

    update schema.rb by new change_collumn migration

--- db/migrate/20190806093420_add_default_value_to_completed_task.rb
+++ db/migrate/20190806093420_add_default_value_to_completed_task.rb
@@ -1,5 +1,5 @@
 class AddDefaultValueToCompletedTask < ActiveRecord::Migration[5.2]
   def change
-    add_column :tasks, :completed, :boolean, default: false
+    change_column :tasks, :completed, :boolean, default: false
   end
 end
